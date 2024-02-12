797ac545f16169dfb8be05ea3a9b8c732a0ddf3a
inlancersystem4 <inlancersystem4@gmail.com>
2024-02-12 19:15:44 +0530

    add dashboard Follow Up List

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,9 +1,16 @@
 <script>
+
+import { useAuthStore, useAlertStore } from '../stores'
+import { fetchWrapper } from '../helpers/fetch-wrapper'
+
 import Layout from '../components/Layout.vue';
-import { useAuthStore } from "@/stores";
+import HomePage from '@/subcomponents/HomePage.vue'
+
+const baseUrl = `${import.meta.env.VITE_API_URL}`;
+
 
 export default {
-    components: { Layout },
+    components: { Layout, HomePage },
     data() {
         return {
             breadcrumbList: [
@@ -11,6 +18,7 @@ export default {
                     name: 'Dashboard',
                 },
             ],
+            list: [],
         }
     },
     created() {
@@ -19,6 +27,32 @@ export default {
         const description = "this is description for Home"
 
         authStore.chnageTitle(title, description)
+        this.reminderData();
+    },
+    methods: {
+        async reminderData() {
+
+            var user_data = new FormData();
+            user_data.append("rem_id", "");
+            user_data.append("sort", "asc");
+            user_data.append("page_no", "1");
+
+            try {
+                const response = await fetchWrapper.post(`${baseUrl}/reminder-list`, user_data);
+
+                if (response.data.length !== 0) {
+                    this.list = response.data;
+                }
+
+            } catch (error) {
+                const alertStore = useAlertStore()
+                alertStore.error(error)
+            }
+
+        },
+        goToFollowUp() {
+            this.$router.push({ name: 'ReminderList' })
+        }
     },
 }
 </script>
@@ -27,192 +61,34 @@ export default {
 <template>
     <Layout :breadcrumb="breadcrumbList">
 
+        <HomePage />
 
-        <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-
-            <div class="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
-                <div>
-                    <p
-                        class="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider  uppercase rounded-full bg-sky-500 text-white">
-                        Real Estate
-                    </p>
-                </div>
-                <h2
-                    class="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
-                    <span class="relative inline-block">
-
-                        <svg viewBox="0 0 52 24" fill="currentColor"
-                            class="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-slate-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block">
-                            <defs>
-                                <pattern id="fdca20a0-aeb4-4caf-ba1b-4351eee42363" x="0" y="0" width=".135" height=".30">
-                                    <circle cx="1" cy="1" r=".7"></circle>
-                                </pattern>
-                            </defs>
-                            <rect fill="url(#fdca20a0-aeb4-4caf-ba1b-4351eee42363)" width="52" height="24"></rect>
-                        </svg>
-
-                        <span class="relative">The</span>
-                    </span>
-                    Billion Build
-                </h2>
-                <p class="text-base text-gray-700 md:text-lg">
-                    One stop real estate CRM for Customers to inventories.Manage All Daily operation like a King...
-                </p>
+        <div class="w-full max-w-sm bg-white border border-solid border-Grey_20 rounded-regualr overflow-hidden">
+            <div class="px-5 py-2 border-b border-solid border-Grey_20">
+                <h2 class="text-large_regular color-Grey_90">Follow Up List</h2>
             </div>
-
-
-            <div class="grid max-w-md gap-8 row-gap-10 sm:mx-auto lg:max-w-full lg:grid-cols-3">
-                <div class="flex flex-col sm:flex-row">
-                    <div class="sm:mr-4">
-                        <div class="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-green-50">
-                            <svg class="w-12 h-12 text-green-400" stroke="currentColor" viewBox="0 0 52 52">
-                                <polygon stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none"
-                                    points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
-                            </svg>
+            <div class="overflow-y-auto max-h-96 relative">
+                <ul>
+                    <li class="border-b border-solid border-Grey_20 px-5 py-2" v-for="(item, index) in list" :key="index">
+                        <div class="w-full flex justify-between items-start gap-3.5">
+                            <div class="space-y-1.5">
+                                <h6 class="color-Grey_90 text-md text-capitalize">{{ item.rem_cus }}</h6>
+                                <p class="color-Grey_50 text-sm text-capitalize">{{ item.rem_notes }}</p>
+                            </div>
+                            <button class="btn-regular display-flex align-center gap-8px" @click="statusShow(index)">
+                                <div class="ellipse-dot"
+                                    :class="{ 'bg-emerald': item.rem_status_id === 3, 'bg-orange': item.rem_status_id === 2, 'bg-Grey_40': item.rem_status_id === 1 }">
+                                </div>
+                                <p class="text-sm_medium color-Grey_60 capitalize">{{ item.rem_status }}</p>
+                            </button>
                         </div>
-                    </div>
-                    <div>
-                        <h6 class="mb-2 font-semibold leading-5">Project Structure:</h6>
-                        <p class="mb-3 text-sm text-gray-900">
-                            Manage your whole project from basic step to final outcome.
-                        </p>
-                        <ul class="mb-4 -ml-1 space-y-2">
-                            <li class="flex items-start">
-                                <span class="mr-1">
-                                    <svg class="w-5 h-5 mt-px text-green-400" stroke="currentColor" viewBox="0 0 52 52">
-                                        <polygon stroke-width="4" stroke-linecap="round" stroke-linejoin="round" fill="none"
-                                            points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
-                                    </svg>
-                                </span>
-                                Project
-                            </li>
-                            <li class="flex items-start">
-                                <span class="mr-1">
-                                    <svg class="w-5 h-5 mt-px text-green-400" stroke="currentColor" viewBox="0 0 52 52">
-                                        <polygon stroke-width="4" stroke-linecap="round" stroke-linejoin="round" fill="none"
-                                            points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
-                                    </svg>
-                                </span>
-                                Area
-                            </li>
-                            <li class="flex items-start">
-                                <span class="mr-1">
-                                    <svg class="w-5 h-5 mt-px text-green-400" stroke="currentColor" viewBox="0 0 52 52">
-                                        <polygon stroke-width="4" stroke-linecap="round" stroke-linejoin="round" fill="none"
-                                            points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
-                                    </svg>
-                                </span>
-                                Structure
-                            </li>
-                        </ul>
-                        <router-link to="/" aria-label=""
-                            class="inline-flex items-center font-semibold transition-colors duration-200 text-green-400 hover:text-green-800">Learn
-                            more
-                        </router-link>
-                    </div>
-                </div>
-                <div class="flex flex-col sm:flex-row">
-                    <div class="sm:mr-4">
-                        <div class="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-green-50">
-                            <svg class="w-12 h-12 text-green-400" stroke="currentColor" viewBox="0 0 52 52">
-                                <polygon stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none"
-                                    points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
-                            </svg>
-                        </div>
-                    </div>
-                    <div>
-                        <h6 class="mb-2 font-semibold leading-5">Customer & Follow Ups:</h6>
-                        <p class="mb-3 text-sm text-gray-900">
-                            Manage customer data, followups, new leads, communication alerts, sales tips.
-                        </p>
-                        <ul class="mb-4 -ml-1 space-y-2">
-                            <li class="flex items-start">
-                                <span class="mr-1">
-                                    <svg class="w-5 h-5 mt-px text-green-400" stroke="currentColor" viewBox="0 0 52 52">
-                                        <polygon stroke-width="4" stroke-linecap="round" stroke-linejoin="round" fill="none"
-                                            points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
-                                    </svg>
-                                </span>
-                                Customers
-                            </li>
-                            <li class="flex items-start">
-                                <span class="mr-1">
-                                    <svg class="w-5 h-5 mt-px text-green-400" stroke="currentColor" viewBox="0 0 52 52">
-                                        <polygon stroke-width="4" stroke-linecap="round" stroke-linejoin="round" fill="none"
-                                            points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
-                                    </svg>
-                                </span>
-                                Lead
-                            </li>
-                            <li class="flex items-start">
-                                <span class="mr-1">
-                                    <svg class="w-5 h-5 mt-px text-green-400" stroke="currentColor" viewBox="0 0 52 52">
-                                        <polygon stroke-width="4" stroke-linecap="round" stroke-linejoin="round" fill="none"
-                                            points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
-                                    </svg>
-                                </span>
-                                Follow Ups
-                            </li>
-                        </ul>
-                        <router-link to="/" aria-label=""
-                            class="inline-flex items-center font-semibold transition-colors duration-200 text-green-400 hover:text-green-800">Learn
-                            more
-                        </router-link>
-                    </div>
-                </div>
-                <div class="flex flex-col sm:flex-row">
-                    <div class="sm:mr-4">
-                        <div class="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-green-50">
-                            <svg class="w-12 h-12 text-green-400" stroke="currentColor" viewBox="0 0 52 52">
-                                <polygon stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none"
-                                    points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
-                            </svg>
-                        </div>
-                    </div>
-                    <div>
-                        <h6 class="mb-2 font-semibold leading-5">Inventories & process</h6>
-                        <p class="mb-3 text-sm text-gray-900">
-                            Track inventory status like Hold, booked, sold, Available with fare prices and other details
-                        </p>
-                        <ul class="mb-4 -ml-1 space-y-2">
-                            <li class="flex items-start">
-                                <span class="mr-1">
-                                    <svg class="w-5 h-5 mt-px text-green-400" stroke="currentColor" viewBox="0 0 52 52">
-                                        <polygon stroke-width="4" stroke-linecap="round" stroke-linejoin="round" fill="none"
-                                            points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
-                                    </svg>
-                                </span>
-                                Inventories (Flat, House, Offices)
-                            </li>
-                            <li class="flex items-start">
-                                <span class="mr-1">
-                                    <svg class="w-5 h-5 mt-px text-green-400" stroke="currentColor" viewBox="0 0 52 52">
-                                        <polygon stroke-width="4" stroke-linecap="round" stroke-linejoin="round" fill="none"
-                                            points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
-                                    </svg>
-                                </span>
-                                Issue & process tracking
-                            </li>
-                            <li class="flex items-start">
-                                <span class="mr-1">
-                                    <svg class="w-5 h-5 mt-px text-green-400" stroke="currentColor" viewBox="0 0 52 52">
-                                        <polygon stroke-width="4" stroke-linecap="round" stroke-linejoin="round" fill="none"
-                                            points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
-                                    </svg>
-                                </span>
-                                Track changes & updates by customers
-                            </li>
-                        </ul>
-                        <router-link to="/" aria-label=""
-                            class="inline-flex items-center font-semibold transition-colors duration-200 text-green-400 hover:text-green-800">Learn
-                            more
-                        </router-link>
-                    </div>
-                </div>
+                    </li>
+                </ul>
             </div>
-
+            <div class="px-5 py-2 bg-white bg-opacity-10 backdrop-blur-md">
+                <button class="btn-regular btn-w-full" @click="goToFollowUp">See all</button>
+            </div>
         </div>
-
 
     </Layout>
 </template>
---
+++ src/subcomponents/HomePage.vue
@@ -0,0 +1,186 @@
+<template>
+    <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+
+        <div class="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+            <div>
+                <p
+                    class="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider  uppercase rounded-full bg-sky-500 text-white">
+                    Real Estate
+                </p>
+            </div>
+            <h2
+                class="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+                <span class="relative inline-block">
+
+                    <svg viewBox="0 0 52 24" fill="currentColor"
+                        class="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-slate-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block">
+                        <defs>
+                            <pattern id="fdca20a0-aeb4-4caf-ba1b-4351eee42363" x="0" y="0" width=".135" height=".30">
+                                <circle cx="1" cy="1" r=".7"></circle>
+                            </pattern>
+                        </defs>
+                        <rect fill="url(#fdca20a0-aeb4-4caf-ba1b-4351eee42363)" width="52" height="24"></rect>
+                    </svg>
+
+                    <span class="relative">The</span>
+                </span>
+                Billion Build
+            </h2>
+            <p class="text-base text-gray-700 md:text-lg">
+                One stop real estate CRM for Customers to inventories.Manage All Daily operation like a King...
+            </p>
+        </div>
+
+
+        <div class="grid max-w-md gap-8 row-gap-10 sm:mx-auto lg:max-w-full lg:grid-cols-3">
+            <div class="flex flex-col sm:flex-row">
+                <div class="sm:mr-4">
+                    <div class="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-green-50">
+                        <svg class="w-12 h-12 text-green-400" stroke="currentColor" viewBox="0 0 52 52">
+                            <polygon stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none"
+                                points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
+                        </svg>
+                    </div>
+                </div>
+                <div>
+                    <h6 class="mb-2 font-semibold leading-5">Project Structure:</h6>
+                    <p class="mb-3 text-sm text-gray-900">
+                        Manage your whole project from basic step to final outcome.
+                    </p>
+                    <ul class="mb-4 -ml-1 space-y-2">
+                        <li class="flex items-start">
+                            <span class="mr-1">
+                                <svg class="w-5 h-5 mt-px text-green-400" stroke="currentColor" viewBox="0 0 52 52">
+                                    <polygon stroke-width="4" stroke-linecap="round" stroke-linejoin="round" fill="none"
+                                        points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
+                                </svg>
+                            </span>
+                            Project
+                        </li>
+                        <li class="flex items-start">
+                            <span class="mr-1">
+                                <svg class="w-5 h-5 mt-px text-green-400" stroke="currentColor" viewBox="0 0 52 52">
+                                    <polygon stroke-width="4" stroke-linecap="round" stroke-linejoin="round" fill="none"
+                                        points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
+                                </svg>
+                            </span>
+                            Area
+                        </li>
+                        <li class="flex items-start">
+                            <span class="mr-1">
+                                <svg class="w-5 h-5 mt-px text-green-400" stroke="currentColor" viewBox="0 0 52 52">
+                                    <polygon stroke-width="4" stroke-linecap="round" stroke-linejoin="round" fill="none"
+                                        points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
+                                </svg>
+                            </span>
+                            Structure
+                        </li>
+                    </ul>
+                    <router-link to="/" aria-label=""
+                        class="inline-flex items-center font-semibold transition-colors duration-200 text-green-400 hover:text-green-800">Learn
+                        more
+                    </router-link>
+                </div>
+            </div>
+            <div class="flex flex-col sm:flex-row">
+                <div class="sm:mr-4">
+                    <div class="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-green-50">
+                        <svg class="w-12 h-12 text-green-400" stroke="currentColor" viewBox="0 0 52 52">
+                            <polygon stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none"
+                                points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
+                        </svg>
+                    </div>
+                </div>
+                <div>
+                    <h6 class="mb-2 font-semibold leading-5">Customer & Follow Ups:</h6>
+                    <p class="mb-3 text-sm text-gray-900">
+                        Manage customer data, followups, new leads, communication alerts, sales tips.
+                    </p>
+                    <ul class="mb-4 -ml-1 space-y-2">
+                        <li class="flex items-start">
+                            <span class="mr-1">
+                                <svg class="w-5 h-5 mt-px text-green-400" stroke="currentColor" viewBox="0 0 52 52">
+                                    <polygon stroke-width="4" stroke-linecap="round" stroke-linejoin="round" fill="none"
+                                        points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
+                                </svg>
+                            </span>
+                            Customers
+                        </li>
+                        <li class="flex items-start">
+                            <span class="mr-1">
+                                <svg class="w-5 h-5 mt-px text-green-400" stroke="currentColor" viewBox="0 0 52 52">
+                                    <polygon stroke-width="4" stroke-linecap="round" stroke-linejoin="round" fill="none"
+                                        points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
+                                </svg>
+                            </span>
+                            Lead
+                        </li>
+                        <li class="flex items-start">
+                            <span class="mr-1">
+                                <svg class="w-5 h-5 mt-px text-green-400" stroke="currentColor" viewBox="0 0 52 52">
+                                    <polygon stroke-width="4" stroke-linecap="round" stroke-linejoin="round" fill="none"
+                                        points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
+                                </svg>
+                            </span>
+                            Follow Ups
+                        </li>
+                    </ul>
+                    <router-link to="/" aria-label=""
+                        class="inline-flex items-center font-semibold transition-colors duration-200 text-green-400 hover:text-green-800">Learn
+                        more
+                    </router-link>
+                </div>
+            </div>
+            <div class="flex flex-col sm:flex-row">
+                <div class="sm:mr-4">
+                    <div class="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-green-50">
+                        <svg class="w-12 h-12 text-green-400" stroke="currentColor" viewBox="0 0 52 52">
+                            <polygon stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none"
+                                points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
+                        </svg>
+                    </div>
+                </div>
+                <div>
+                    <h6 class="mb-2 font-semibold leading-5">Inventories & process</h6>
+                    <p class="mb-3 text-sm text-gray-900">
+                        Track inventory status like Hold, booked, sold, Available with fare prices and other details
+                    </p>
+                    <ul class="mb-4 -ml-1 space-y-2">
+                        <li class="flex items-start">
+                            <span class="mr-1">
+                                <svg class="w-5 h-5 mt-px text-green-400" stroke="currentColor" viewBox="0 0 52 52">
+                                    <polygon stroke-width="4" stroke-linecap="round" stroke-linejoin="round" fill="none"
+                                        points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
+                                </svg>
+                            </span>
+                            Inventories (Flat, House, Offices)
+                        </li>
+                        <li class="flex items-start">
+                            <span class="mr-1">
+                                <svg class="w-5 h-5 mt-px text-green-400" stroke="currentColor" viewBox="0 0 52 52">
+                                    <polygon stroke-width="4" stroke-linecap="round" stroke-linejoin="round" fill="none"
+                                        points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
+                                </svg>
+                            </span>
+                            Issue & process tracking
+                        </li>
+                        <li class="flex items-start">
+                            <span class="mr-1">
+                                <svg class="w-5 h-5 mt-px text-green-400" stroke="currentColor" viewBox="0 0 52 52">
+                                    <polygon stroke-width="4" stroke-linecap="round" stroke-linejoin="round" fill="none"
+                                        points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
+                                </svg>
+                            </span>
+                            Track changes & updates by customers
+                        </li>s
+                    </ul>
+                    <router-link to="/" aria-label=""
+                        class="inline-flex items-center font-semibold transition-colors duration-200 text-green-400 hover:text-green-800">Learn
+                        more
+                    </router-link>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
